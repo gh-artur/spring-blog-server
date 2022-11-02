e4ba8b8953a74333ec3104473d662a984c3d0d95
Artur Santos <arturfrancisco86@gmail.com>
2022-11-02 15:26:38 -0300

    Post entity

---
+++ src/main/java/com/ghartur/blogserver/entity/Post.java
@@ -0,0 +1,26 @@
+package com.ghartur.blogserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(
+        name = "posts",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"title"})) //define que não terão dois posts com o mesmo titulo
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String content;
+}
